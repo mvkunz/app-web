642ed1675359ee13181d63677ac3e66dd49e7cc0
Maria Vitória Kunz <mvkunz.dev@gmail.com>
2024-03-04 15:58:29 -0300

    filters

--- frontend/src/Components/ProductFilter.jsx
+++ frontend/src/Components/ProductFilter.jsx
@@ -1,15 +1,39 @@
 import { useState } from "react";
+import Table from "./Table";
 
 function ProductFilter() {
   const [dropdownVisible, setDropdownVisible] = useState(false);
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
 
   const toggleDropdown = () => {
     setDropdownVisible(!dropdownVisible);
   }
 
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  // função para buscar produtos
+  const handleSearch = async () => {
+    try {
+      // faz uma solicitação a API
+      const response = await fetch(`https://sua-api.com/search?q=${query}`);
+      if (!response.ok) {
+        throw new Error('Falha ao buscar dados');
+      }
+      const data = await response.json();
+      // atualiza o estado com os resultados da pesquisa
+      setResults(data);
+    } catch (error) {
+      console.error('Erro ao buscar dados:', error);
+    }
+  };
+
   return (
     <form className="mb-10">
-      <div className="flex">
+      <div className="flex relative">
+        {/* CAMPO DROPDOWN */}
         <button
           id="dropdown-button"
           data-dropdown-toggle="dropdown"
@@ -40,14 +64,46 @@ function ProductFilter() {
             </li>
             </ul>
           </div>
+          {/* CAMPO PESQUISAR */}
           <div className="relative w-full">
-              <input type="search" id="search-dropdown" className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg rounded-s-gray-100 rounded-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search" required />
-              <button type="submit" className="absolute top-0 end-0 p-2.5 h-full text-sm font-medium text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                  <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                </svg>
-              </button>
+            <input
+              type="search"
+              id="search-dropdown"
+              value={ query }
+              onChange={ handleInputChange}
+              className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg rounded-s-gray-100 rounded-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+              placeholder="Search"
+              required />
+            <button
+              type="submit"
+              onClick={ handleSearch }
+              className="absolute top-0 end-0 p-2.5 h-full text-sm font-medium text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+              </svg>
+            </button>
           </div>
+        </div>
+        {/* RESULTADOS DA PESQUISA */}
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <Table />
+              </tr>
+            </thead>
+            <tbody>
+              {results.map((product, index) => {
+                <tr key={index}>
+                  <td>{ product.name }</td>
+                  <td>{ product.brand }</td>
+                  <td>{ product.model }</td>
+                  <td>{ product.price }</td>
+                  <td>{ product.color }</td>
+                </tr>
+              })}
+            </tbody>
+          </table>
         </div>
       </form>
   )
