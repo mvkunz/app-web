1d9ba7702c1c50a254bf46bfd9988112daf0e2fd
Maria Vitória Kunz <mvkunz.dev@gmail.com>
2024-03-05 12:01:39 -0300

    att

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,13 @@ const authRoutes = require("./routes/authRoutes");
 
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./swaggerConfig");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json()); // Middleware para parsear JSON
+app.use(cors());
 
 // Rota base para
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
